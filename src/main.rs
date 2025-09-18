cac38d1cb4701e8871f81489f9acd8855606dc4f
Shagun Agrawal <agrawalshagun07@gmail.com>
2025-09-18 22:09:55 +0530

    Use file input for program source

--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,46 @@ mod lexer;
 mod token;
 
 use crate::{eval::Evaluator, lexer::Lexer};
+use std::{env, fs, process};
 
 fn main() {
-    let source = "1 2 +";
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        eprintln!("Usage: {} <file-path>", args[0]);
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let source: Vec<String> = fs::read_to_string(file_path)
+        .unwrap_or_else(|err| {
+            eprintln!("Error reading {}: {}", file_path, err);
+            process::exit(1);
+        })
+        .lines()
+        .map(|line| line.trim().to_string())
+        .collect();
+
     let lexer = Lexer::new();
-    let tokens = lexer.tokenize(source).unwrap_or_else(|err| {
-        println!("{}", err);
-        std::process::exit(1);
-    });
-
     let mut evaluator = Evaluator::new();
-    let result = evaluator.eval_all(&tokens).unwrap_or_else(|err| {
-        println!("{}", err);
-        std::process::exit(1);
-    });
 
-    println!("{}", match result {
-        Some(t) => format!("{:?}", t),
-        None => "".to_string(),
-    });
+    for line in source {
+        let tokens = lexer.tokenize(&line).unwrap_or_else(|err| {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        });
+
+        let result = evaluator.eval_all(&tokens).unwrap_or_else(|err| {
+            eprintln!("{}", err);
+            std::process::exit(1);
+        });
+
+        println!(
+            "{}",
+            match result {
+                Some(t) => format!("{:?}", t),
+                None => "".to_string(),
+            }
+        );
+    }
 }
